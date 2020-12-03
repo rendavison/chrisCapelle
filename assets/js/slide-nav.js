727f9bcca3d9e6daa072c5dd6b8c0cb940538eeb
Remy <davisonren@gmail.com>
2020-12-03 11:06:37 -0500

    dropdown on click for mobile

--- assets/js/slide-nav.js
+++ assets/js/slide-nav.js
@@ -1,6 +1,5 @@
 //Tutorial by W3Schools
 //https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_sidenav
-
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
 }
@@ -8,3 +7,8 @@ function openNav() {
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
+
+//make the dropdown hover a click on mobile
+$(".dropdown").click(function(){
+    $(this).children(".drop-content").slideToggle("fast");
+});
