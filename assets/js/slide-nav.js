6d718cfa03c8f365cf048717f9d22b53d0728fa0
Remy <davisonren@gmail.com>
2020-12-03 10:41:55 -0500

    added slide nav

---
+++ assets/js/slide-nav.js
@@ -0,0 +1,10 @@
+//Tutorial by W3Schools
+//https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_sidenav
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
